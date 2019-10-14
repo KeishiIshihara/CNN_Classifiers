f4a9c324f0e29ccc4797eccc48bcba68f127933a
KeishiIshihara <ishihara@aisl.cs.tut.ac.jp>
2019-10-14 15:45:24 +0300

    add; add function which plots confusion matrix using matplotlib

--- callbacks.py
+++ callbacks.py
@@ -3,11 +3,11 @@
 #
 #  (c) Keishi Ishihara
 # ------------------------------------------
+
 import os
 import numpy as np
 from  keras.callbacks import Callback
 import matplotlib.pyplot as plt
-
 
 class LearningHistoryCallback(Callback):
     """Callback class for visualizing training curves"""
@@ -33,7 +33,7 @@ class LearningHistoryCallback(Callback):
         self.train_acc.append(logs.get('accuracy'))
         self.val_losses.append(logs.get('val_loss'))
         self.val_acc.append(logs.get('val_accuracy'))
-        
+
         # Before plotting ensure at least 2 epochs have passed
         if epoch >= 1:
 
@@ -62,3 +62,69 @@ class LearningHistoryCallback(Callback):
             plt.savefig('results/{}_result.png'.format(self.prefix))
             plt.close()
 
+
+
+# ------------------------------------------------------
+#  plot confusion matrix using matplotlib
+#  by modifying following referece:
+#
+#  Reference: Confusion matrix — scikit-learn 0.21.3 documentation
+#  https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
+# ------------------------------------------------------
+
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          prefix='test',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    # fig.tight_layout()
+
+    plt.savefig('results/{}_confusion_matrix.png'.format(prefix))
+    return ax
